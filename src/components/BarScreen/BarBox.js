f5009fb2089911c13aa1df922c6fcaae2aee0c22
RylanChamberlin <rylanc88@gmail.com>
2022-04-14 01:14:19 -0500

    added - more info:

--- src/components/BarScreen/BarBox.js
+++ src/components/BarScreen/BarBox.js
@@ -4,75 +4,130 @@ import StarButton from './StarButton';
 import { Dimensions } from 'react-native';
 import {elevation} from "../../common/styles";
 import LikeButton from "./LikeButton";
+import { useState } from "react";
 
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
+
+
 export default function BarBox({name, review_count, specials, events, imageUrl}){
 
-
-    
+    const [more, setMore] = useState(true);
 
     return (
-        <View style = {styles.infoBox}>
-            <View style = {styles.infoBoxLeft}>
-                <Text style = {styles.title}>{name}</Text>
-                <View style = {styles.reviews}>
-                    <StarButton/>
-                    <StarButton/>
-                    <StarButton/>
-                    <StarButton/>
-                    <StarButton/>
-                    <Text style = {styles.reviewNumber}>{review_count}</Text>
-                </View>
-
-                <View style = {styles.specialsContainer}>
-                    <Text style={styles.specialsTitle}>TODAY'S SPECIALS:</Text>
-                    <Text style={styles.specialsText}>{specials}</Text>
-                </View>
-
-                <View style = {styles.eventContainer}>
-                    <Text style={styles.eventTitle}>TODAY'S EVENTS:</Text>
-                    <Text style={styles.eventText}>{events}</Text>
-                </View>
-
-                <TouchableOpacity onPress={() => console.warn('More Button pressed')}>
-                    <View style = {styles.moreContainer}>
-                        <Text style={styles.moreText}>more </Text>
-                        <Text style={styles.moreArrow}>ˇ</Text>
+        <View style={styles.container}>
+            <View style = {styles.infoBox}>
+                <View style = {styles.infoBoxLeft}>
+                    <Text style = {styles.title}>{name}</Text>
+                    <View style = {styles.reviews}>
+                        <StarButton/>
+                        <StarButton/>
+                        <StarButton/>
+                        <StarButton/>
+                        <StarButton/>
+                        <Text style = {styles.reviewNumber}>{review_count}</Text>
                     </View>
-                </TouchableOpacity>
-            </View>
 
-            <View style={styles.infoBoxRight}>
-                <View style={styles.likeButton}>
-                    <LikeButton/>
+                    <View style = {styles.specialsContainer}>
+                        <Text style={styles.specialsTitle}>TODAY'S SPECIALS:</Text>
+                        <Text style={styles.specialsText}>{specials}</Text>
+                    </View>
+
+                    <View style = {styles.eventContainer}>
+                        <Text style={styles.eventTitle}>TODAY'S EVENTS:</Text>
+                        <Text style={styles.eventText}>{events}</Text>
+                    </View>
+
+                    <TouchableOpacity onPress={() => setMore((isMore) => !isMore)}>
+                        {more ? moreButton() : <View style = {styles.moreContainer}><Text style={styles.moreArrow}>More Info:</Text></View>} 
+                    </TouchableOpacity>
+                    
                 </View>
-                <Image style = {styles.image} source={imageUrl}/>
+
+                <View style={styles.infoBoxRight}>
+                    <View style={styles.likeButton}>
+                        <LikeButton/>
+                    </View>
+                    <Image style = {styles.image} source={imageUrl}/>
+
+                    <View>
+                        <TouchableOpacity onPress={() => console.warn('Reserve Button pressed')}>
+                            <View style={[styles.buttonStyle, styles.elevation]}>
+                                <Text style={styles.buttonTextStyle}>RESERVE</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
 
                 <View>
-                    <TouchableOpacity onPress={() => console.warn('Simple Button pressed')}>
-                        <View style={[styles.buttonStyle, styles.elevation]}>
-                            <Text style={styles.buttonTextStyle}>RESERVE</Text>
-                        </View>
-                    </TouchableOpacity>
+                    {!more ? moreText(() => setMore((isMore) => !isMore)) : null} 
                 </View>
-            </View>
-
-            
+               
         </View>
     );
 
+}
+
+function moreButton(){
+    return( 
+        <View style = {styles.moreContainer}>
+            <Text style={styles.moreText}>more </Text>
+            <Text style={styles.moreArrow}>ˇ</Text>
+        </View> 
+    );
+}
+
+function lessButton(){
+    return(
+        <View style = {styles.moreContainer}>
+            <Text style={styles.moreArrow}>^</Text>
+        </View>      
+    );
+}
+
+function moreText(onClick){
+    return(
+        <View>
+
+            <View style = {styles.moreTextContainer}>
+                <Text>Hello</Text>
+                <Text>Hello</Text>
+                <Text>Hello</Text>
+                <Text>Hello</Text>
+                <Text>Hello</Text>
+                <Text>Hello</Text>
+                
+            </View>
+            <TouchableOpacity onPress={onClick}>
+                {lessButton(onClick)}
+            </TouchableOpacity>     
+        </View>
+    );
 }
 
 const infoBoxWidth = windowWidth-30;
 const infoBoxHeight = windowHeight/4;
 
 const styles = StyleSheet.create({
-    infoBox: {
+
+    container: {
+        borderRadius: 8,
         marginTop: 12,
         marginHorizontal: 15,
+        backgroundColor: "rgb(249,249,249)",
+        width: infoBoxWidth,
+        
+    },
+    moreTextContainer:{
+        marginHorizontal: 15,
+        //marginTop: 40
+    },
+    infoBox: {
+        //marginTop: 12,
+        //marginHorizontal: 15,
         borderRadius: 8,
         backgroundColor: "rgb(249,249,249)",
         flexDirection: "row",
@@ -134,6 +189,7 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         marginLeft: 10,
         marginTop: 15,
+        //backgroundColor: "green"
         
     },
 
