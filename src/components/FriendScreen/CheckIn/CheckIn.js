bc929808a0feb19a1f09f06de797ec7c68b30222
RylanChamberlin <rylanc88@gmail.com>
2022-04-30 00:57:16 -0500

    add: styled checkin

--- src/components/FriendScreen/CheckIn/CheckIn.js
+++ src/components/FriendScreen/CheckIn/CheckIn.js
@@ -1,13 +1,33 @@
-import { StyleSheet, Text} from "react-native";
+import { Image, StyleSheet, Text, TextInput, View} from "react-native";
 import { auth } from "../../../firebase";
 import PopupPost from "../../general/PopupPost/PopupPost";
+import styles from "./styles";
 
 
 export default function CheckIn({post, setPost}){
     return(
         <PopupPost post={post} setPost={setPost} title={'CHECK-IN'} buttonTitle={'CHECK-IN'}>
-            <Text style={{color: 'red'}}>{auth.currentUser?.email}</Text>
-            <Text style={{color: 'red'}}>{auth.currentUser?.name}</Text>
+
+        <View style={styles.container}>
+            <View style={styles.userBox}>
+                <Image style={styles.image}/>
+                <View style={styles.nameBox}>
+                    <Text style={styles.name}>Rylan Chamberlin</Text>
+                    <Text style={styles.username}>@pretty.boy.3</Text>
+                </View>
+            </View>
+            <TextInput style={styles.input} placeholder='is at....'></TextInput>
+            
+            <Text style={{color: 'red'}}>NEARBY</Text>
+            <View style={styles.barnameBox}>
+                <Text style={styles.barName}>Harpo's </Text>
+                <Text style={styles.cityName}>Columbia, MO</Text>
+            </View>
+            <View style={styles.barnameBox}>
+                <Text style={styles.barName}>The Shot Bar </Text>
+                <Text style={styles.cityName}>Columbia, MO</Text>
+            </View>
+        </View>
         </PopupPost>        
     );
 }
