ebd4c40a9ebf057dcdb18e83224fbf7bdbbdc2b0
RylanChamberlin <rylanc88@gmail.com>
2022-06-29 20:24:35 -0500

    add: searchbar for friends

---
+++ frontend/src/components/general/SearchFriend/SearchFriend.tsx
@@ -0,0 +1,24 @@
+import { Dispatch, FC, SetStateAction, useState } from "react";
+import { TextInput } from "react-native"
+import styles from "./styles";
+
+type SearchFriendProps = {
+    query: string
+    setQuery: Dispatch<SetStateAction<string>>
+    placeholder: string
+}
+
+const SearchFriend: FC<SearchFriendProps> = ({query, setQuery, placeholder}) => {
+
+    return (
+        <TextInput
+            placeholder={placeholder}
+            style={styles.input}
+            value={query} 
+            onPressIn={() => {setQuery('')}}
+            onChangeText={(text) => {setQuery(text)}}
+        />
+    )
+}
+
+export default SearchFriend
---
+++ frontend/src/components/general/SearchFriend/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  
+    input: {
+        padding: 10,
+        backgroundColor: "white",
+        borderRadius: 20,
+        margin: 10
+    },
+
+})
