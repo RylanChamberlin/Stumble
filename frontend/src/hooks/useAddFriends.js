aa5c39c8ddcc8cb071754cbbad2b66e72e8f5d5a
RylanChamberlin <rylanc88@gmail.com>
2022-07-10 19:25:00 -0500

    fix: friend item disappears when you add them

--- frontend/src/hooks/useAddFriends.js
+++ frontend/src/hooks/useAddFriends.js
@@ -3,101 +3,70 @@ import { auth, db } from '../firebase';
 
 export default () => {
 
-    const [names, setNames] = useState([]);
-    const [usernames, setUsernames] = useState([]); 
+    const [users, setUsers] = useState([]);
     const [list, setList] = useState([])
-    const [oldList , setOldList] = useState([])
-    const [userFriends , setUserFriends] = useState([]);
+
+    useEffect( () => {
+
+        if(!users.length) return;
+        const names = users.map(user => user.username)
+        const subscriber = db
+            .collection("users")
+            .doc(auth.currentUser.uid)
+            .collection('Friends')
+            .where("username", "in", names)
+            .onSnapshot((snapshot, error) => {
+                console.log('snapshot: getting users friends doc in list')
+                let friends = snapshot.docs.map(doc => {
+                    const data = doc.data();
+                    const id = doc.id;
+                    return { id, ...data }
+                })
+                const userNotSent = users.filter(function(objFromA) { 
+                    return !friends.find(function(objFromB) {
+                        return objFromA.id === objFromB.id
+                        })
+                    })
+        
+                setList(userNotSent)
+                console.log(friends)
+            })
+
+            
+
+        return () => subscriber()
+
+    },[users])
     
     const queryUsers = async (name) => {
 
-        console.log('query name firebase')
+        const queryNames = await db.collection('users').where('name', '>=', name).limit(2).get()
+        const queryUsernames = await db.collection('users').where('username', '>=', name).limit(2).get()
 
-        queryUsersByName(name)
-        queryUsersByUsername(name)
-
-        const ids = new Set(usernames.map(d => d.id));
-        const merged = [...usernames, ...names.filter(d => !ids.has(d.id))]
-
-        let listOfUsernames = merged.map((friend) => {
-                return friend.username
-            })
-
-        fetchUserFriend(listOfUsernames)
-
-        const userNotSent = merged.filter(function(objFromA) { 
-            return !userFriends.find(function(objFromB) {
-                return objFromA.id === objFromB.id
-            })
-        })
-
-        setList(userNotSent)
-    }
-
-    const fetchUserFriend = async(list) => {
-
-        if(!list.length) return;
-
-        if(list.length === oldList.length && list.every(function (element) {return oldList.includes(element);})) return;
-        
-        setOldList(list)
-    
-        console.log('newquery\n')
-        const query = await db.collection("users").doc(auth.currentUser.uid).collection("Friends").where("username", "in", list).get()
-
-        let f = query.docs.map(doc => {
+        let names = queryNames.docs.map(doc => {
             const data = doc.data();
             const id = doc.id;
             return { id, ...data }
         })
 
-        setUserFriends(f)
+        let usernames = queryUsernames.docs.map(doc => {
+            const data = doc.data();
+            const id = doc.id;
+            return { id, ...data }
+        })
+
+       
+        const ids = new Set(usernames.map(d => d.id));
+        const mergedUsers = [...usernames, ...names.filter(d => !ids.has(d.id))]
+ 
+        
+        if(!mergedUsers.length) return;
+        if(users.length === mergedUsers.length && mergedUsers.every(c => users.find(e=>e.id==c.id)))return
+        setUsers(mergedUsers)
        
     }
 
-    const queryUsersByName = (name) => {
-        if (!name.length) {
-            return
-        }
 
-
-        console.log('fetch friend list')
-        db
-            .collection('users')
-            .where('name', '>=', name)
-            .limit(2)
-            .get()
-            .then((snapshot) => {
-                let users = snapshot.docs.map(doc => {
-                    const data = doc.data();
-                    const id = doc.id;
-                    return { id, ...data }
-                });
-
-               
-                setNames(users)
-            })
-    }
-
-    const queryUsersByUsername = (username)  => {
-       
-        if (!username.length) {
-            return
-        }
-        db
-            .collection('users')
-            .where('username', '>=', username)
-            .limit(2)
-            .get()
-            .then((snapshot) => {
-                let users = snapshot.docs.map(doc => {
-                    const data = doc.data();
-                    const id = doc.id;
-                    return { id, ...data }
-                });
-                setUsernames(users)
-            })   
-    }
 
     return {queryUsers, list}
 
