d6befb585db50861e8cb429e20bfd9bf1f6f194a
RylanChamberlin <rylanc88@gmail.com>
2022-09-03 22:11:47 -0500

    remove friends features useHooks

--- frontend/src/hooks/useNearby.js
+++ frontend/src/hooks/useNearby.js
@@ -1,9 +1,7 @@
 
 import {GOOGLE_KEY} from '@env'
 
-import { doc, serverTimestamp, updateDoc } from 'firebase/firestore';
 import { useState } from 'react';
-import { auth, db } from '../firebase';
 import { useAppSelector } from '../app/hooks';
 
 
@@ -14,23 +12,7 @@ export default () => {
     const [isError, setIsError] = useState(false);
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false)
-    const searchNearby = async (barInput) => {
-
-        if(location && barInput){
-            setIsLoading(true)
-            const latitude = location.coords.latitude;
-            const longitude = location.coords.longitude;
-            await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude}%2C${longitude}&radius=16000&type=bar&keyword=${barInput}&key=${GOOGLE_KEY}`)
-            .then(response => response.json())
-            .then(json => setData(json.results)) 
-            .catch(error => setIsError(error))
-            console.log('feethcing google nearby api')
-        }
-
-        setIsLoading(false)
-
-    }
-
+    
     const searchNearbyPhone = async (radius = 16000) => {
 
         if(location){
@@ -48,29 +30,6 @@ export default () => {
 
     }
 
-
-    const checkIn = async(bar) => {
-        console.log(bar)
-        let check = {
-            lat: bar.geometry.location.lat,
-            lng: bar.geometry.location.lng,
-            locationID: bar.place_id,
-            locationName: bar.name
-        }
-        try {
-            
-            const userRef = doc(db, "users", auth.currentUser.uid);
-            await updateDoc(userRef, {
-                checkIn: check,
-                checkInTime: serverTimestamp()
-            });
-        } catch (e) {
-            console.error("Error adding document: ", e);
-        }
-
-    }
-
-   
-    return {isLoading, data, isError, searchNearby, searchNearbyPhone, checkIn}
+    return {isLoading, data, isError, searchNearbyPhone, checkIn}
 
 };
