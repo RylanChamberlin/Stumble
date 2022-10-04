d23335a2776848d4b5a0794696974d4bf01897db
RylanChamberlin <rylanc88@gmail.com>
2022-10-04 16:53:14 -0500

    gets location on newpost click

--- frontend/src/screens/Posts/NewPostScreen.tsx
+++ frontend/src/screens/Posts/NewPostScreen.tsx
@@ -1,14 +1,30 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { Platform } from "react-native";
+import { useAppDispatch } from "../../app/hooks";
 import AppView from "../../components/general/AppView";
 import NewPostBox from "../../components/NewPostScreen/NewPostBox";
+import { storeLocation } from "../../features/Location/locationSlice";
 import { RootStackParamList } from "../../navigation/types";
+import { fetchLocation } from "../../services/fetchLocation";
 
 type NewPostScreenProps = NativeStackScreenProps<RootStackParamList, 'NewPostScreen'>;
 
 const NewPostScreen: FC<NewPostScreenProps> = ({route}) => {
+
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        getNewLocation()
+    },[])
+
+
+    const getNewLocation = async() => {
+        console.log('getting new location')
+        let location = await fetchLocation()
+        dispatch(storeLocation(location));
+    }
 
     return(
         <AppView>
